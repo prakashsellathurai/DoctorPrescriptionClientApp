d56db0960cfc2d3180aa7c9340f2a6d84655096f
prakashsellathurai <prakash1729brt@gmail.com>
2018-03-21 03:17:50 +0530

    base architecture

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { CheckoutComponent } from './component/checkout/checkout.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule
